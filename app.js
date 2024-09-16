e141a118c5d7cb1d34f9ea73f0f7dedad2b7121c
Gowtham271199 <gothammelo27@gmail.com>
2024-09-16 15:49:52 +0530

    server check

--- app.js
+++ app.js
@@ -10,7 +10,9 @@ connectDB();
 
 const app = express();
 app.use(express.json());
-
+app.get('/', (req, res) => {
+    res.status(200).json({ message: 'Server is running' });
+  });
 app.use('/api/auth', authRoutes);
 app.use('/api', surveyRoutes);
 
